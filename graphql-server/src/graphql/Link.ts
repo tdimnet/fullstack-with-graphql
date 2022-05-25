65649ae9ca6a3e4094faa7c384100fb25dcbf896
tdimnet <thomas.dimnet@datadoghq.com>
2022-05-25 11:09:25 +0200

    Finish the backend part

--- graphql-server/src/graphql/Link.ts
+++ graphql-server/src/graphql/Link.ts
@@ -52,6 +52,17 @@ const LinkOrderByInput = inputObjectType({
     }
 })
 
+const Feed = objectType({
+    name: 'Feed',
+    definition(t) {
+        t.nonNull.list.nonNull.field('links', {
+            type: Link
+        })
+        t.nonNull.int('count')
+        t.id('id')
+    }
+})
+
 const Sort = enumType({
     name: 'Sort',
     members: ['asc', 'desc']
@@ -60,15 +71,15 @@ const Sort = enumType({
 const LinkQuery = extendType({
     type: 'Query',
     definition(t) {
-        t.nonNull.list.nonNull.field('feed', {
-            type: 'Link',
+        t.nonNull.field('feed', {
+            type: 'Feed',
             args: {
                 filter: stringArg(),
                 skip: intArg(),
                 take: intArg(),
                 orderBy: arg({ type: list(nonNull(LinkOrderByInput)) })
             },
-            resolve(parent, args, context) {
+            async resolve(parent, args, context) {
                 const where = args.filter
                     ? {
                         OR: [
@@ -86,12 +97,21 @@ const LinkQuery = extendType({
                     }
                     : {}
 
-                return context.prisma.link.findMany({
+                const links = await context.prisma.link.findMany({
                     where,
                     skip: args?.skip as number | undefined,
                     take: args?.take as number | undefined,
                     orderBy: args?.orderBy as Prisma.Enumerable<Prisma.LinkOrderByWithRelationInput> | undefined
                 })
+
+                const count = await context.prisma.link.count({ where })
+                const id = `main-feed:${JSON.stringify(args)}`
+
+                return {
+                    links,
+                    count,
+                    id
+                }
             }
         })
     }
@@ -133,6 +153,7 @@ const LinkMutation = extendType({
 })
 
 export {
+    Feed,
     Link,
     LinkMutation,
     LinkOrderByInput,
